19614750f92743a2b5cfbeeaa9f6fef22f407f6f
anyel-ec <paulangel826@gmail.com>
2024-06-12 19:22:41 -0500

    add config of CORS

---
+++ src/main/java/com/liseth/angular/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.liseth.angular.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*permitir que todos puedan realizar peticiones de API*/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
